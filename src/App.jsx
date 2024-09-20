76b58c7890f9d7342fbe4a67dc772e8af6638d88
Sanidhya786 <kumarsanidhya02@gmail.com>
2024-09-20 07:43:03 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  // javascript logic
+
+  return (
+    <div>
+        <div>
+            {/* always return HTML */}
+            <h1>Hello, Reactor</h1>
+        </div>
+        <div>
+            <h1>I'm Arch</h1>
+        </div>
+    </div>
+  )
+}
+export default App;
